8b67b0d99f6eea217b618c8910c180e15d69ab52
chenjunqian <chenjunqian0810@foxmail.com>
2023-11-19 03:20:23 +0800

    add mini map

---
+++ lua/plugins/mini-map.lua
@@ -0,0 +1,10 @@
+return {
+    "echasnovski/mini.map",
+    key = {
+        {
+            "<leader>lm",
+            "<cmd>MiniMap.toggle()<cr>",
+            desc = "Toggle Mini Map",
+        },
+    },
+}
