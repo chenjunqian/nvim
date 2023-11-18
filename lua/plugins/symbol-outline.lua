ede07f0d3b4041f36e4829460558a0c32446407e
chenjunqian <chenjunqian0810@foxmail.com>
2023-11-19 03:20:39 +0800

    add outline config

---
+++ lua/plugins/symbol-outline.lua
@@ -0,0 +1,6 @@
+return {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>lo", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    config = true,
+}
