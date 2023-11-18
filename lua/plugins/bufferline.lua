3acb300473da27c8b9e38819aa719ebfddcedb5e
chenjunqian <chenjunqian0810@foxmail.com>
2023-11-19 03:21:01 +0800

    update keymap

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,8 +2,8 @@ return {
     "akinsho/bufferline.nvim",
     keys = {
         { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
-        { "<leader>bh", "<Cmd>BufferLineCloseRight<CR>",  desc = "Delete buffers to the right" },
-        { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>",   desc = "Delete buffers to the left" },
+        { "<leader>bl", "<Cmd>BufferLineCloseRight<CR>",  desc = "Delete buffers to the right" },
+        { "<leader>bh", "<Cmd>BufferLineCloseLeft<CR>",   desc = "Delete buffers to the left" },
         { "<s-tab>",    "<cmd>BufferLineCyclePrev<cr>",   desc = "Prev buffer" },
         { "<tab>",      "<cmd>BufferLineCycleNext<cr>",   desc = "Next buffer" },
     },
