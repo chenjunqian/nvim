33b3c933cf91610b8c0e898ccb4742c049c3243d
chenjunqian <chenjunqian0810@foxmail.com>
2023-11-19 15:18:18 +0800

    add golang lsp config

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -9,8 +9,8 @@ return {
                 "rcarriga/nvim-dap-ui",
                 -- stylua: ignore
                 keys = {
-                    { "<leader>du", function() require("dapui").toggle({}) end,  desc = "Dap UI" },
-                    { "<leader>de", function() require("dapui").eval() end,      desc = "Eval",  mode = { "n", "v" } },
+                    { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI" },
+                    { "<leader>de", function() require("dapui").eval() end,     desc = "Eval",  mode = { "n", "v" } },
                 },
                 opts = {},
                 config = function(_, opts)
