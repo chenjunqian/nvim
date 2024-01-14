ac04c614dc1105ebaf6caaa0529914e2fd66de13
chenjunqian <chenjunqian0810@foxmail.com>
2024-01-14 14:58:16 +0800

    format

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,9 +8,10 @@ local opts = { noremap = true, silent = true }
 keymap.set("n", "L", "$", opts)
 keymap.set("n", "H", "^", opts)
 
-keymap.del("n","<Leader>/")
+keymap.del("n", "<Leader>/")
+
 -- LSP
-keymap.del("n","<Leader>l")
+keymap.del("n", "<Leader>l")
 keymap.set("n", "<Leader>lr", vim.lsp.buf.rename, { desc = "Rename" })
 keymap.set("n", "<Leader>la", vim.lsp.buf.code_action, { desc = "Code Action" })
 keymap.set("n", "<Leader>lf", vim.lsp.buf.format, { desc = "Format" })
@@ -21,16 +22,16 @@ keymap.set("n", "<Leader>ld", vim.diagnostic.open_float, { desc = "Line Diagnost
 local Util = require("lazyvim.util")
 keymap.set("n", "<Leader>Ll", "<cmd>Lazy<cr>", { desc = "Lazy Vim" })
 keymap.set("n", "<Leader>LL", function()
-  Util.news.changelog()
+    Util.news.changelog()
 end, { desc = "LazyVim Changelog" })
 
 -- floating terminal
 local lazyterm = function()
-  Util.terminal(nil, { cwd = Util.root() })
+    Util.terminal(nil, { cwd = Util.root() })
 end
 keymap.set("n", "<leader>ft", lazyterm, { desc = "Terminal (root dir)" })
 keymap.set("n", "<leader>fT", function()
-  Util.terminal()
+    Util.terminal()
 end, { desc = "Terminal (cwd)" })
 keymap.set("n", "<c-\\>", lazyterm, { desc = "Terminal (root dir)" })
 keymap.set("t", "<c-\\>", "<cmd>close<cr>", { desc = "Close Terminal" })
